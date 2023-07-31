5189bc6ccee4f41a999bf55790f99744dfadd80e
Igor Natan <igornatan4@gmail.com>
2023-07-31 17:24:25 -0300

    commit component footer

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css'
+
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer>
+        
+    </footer>
+  )
+}
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,10 @@
 import Cabecalho from 'components/Cabecalho'
+import Rodape from 'components/Rodape'
 import React from 'react'
 
 export default function Inicio() {
   return (
     <Cabecalho />
+    <Rodape />
   )
 }
